3a8bc2a525a304b24c0a99a60e85e94ffe3c157f
Yury Ghirin <yury.ghirin@gmail.com>
2025-12-31 12:19:36 +0000

    Add changelog for CRUD interface release

---
+++ patients/templatetags/patient_tags.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def safe_username(user):
+    """Безопасное получение username или full_name"""
+    if user:
+        return user.get_full_name() or user.username
+    return "Не указан"
+
+@register.filter
+def safe_getattr(obj, attr_name, default="Не указан"):
+    """Безопасное получение атрибута"""
+    if hasattr(obj, attr_name):
+        value = getattr(obj, attr_name)
+        if callable(value):
+            value = value()
+        return value if value is not None else default
+    return default
